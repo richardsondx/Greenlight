19256f51bfdf7323333aa864f7f277eb72b7d9cc
evenix <richardsondx@gmail.com>
2012-11-15 03:52:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greenlight::Application.config.secret_token = '8549818dc6a8cf41616e572f6d8c6494fb5180aa05f20be93cf70c9f6ae22f9f122789a65b328298e94a32247f80e2fe03acf15d606649a9e58f462acf2912fb'
